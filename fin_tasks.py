2926c11267952aaf4ff7655f38d8772cade614ce
Abhinandan Pakhare <abhinandanpakhare@gmail.com>
2026-07-13 20:58:22 +0530

    Update fin_tasks.py

--- fin_tasks.py
+++ fin_tasks.py
@@ -3,7 +3,7 @@ from crewai import Task
 # from agents import location_expert, guide_expert, planner_expert
 #DuckDuckGoSearchRunTool DuckDuckGoSearchResults DuckDuckGoSearchRun
 from langchain_groq import ChatGroq
-from template import fin_template, news_template
+import fin_template, news_template
 
 
 
