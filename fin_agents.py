28dc7aabbc3da2ae5f0160e3a0b6ff5807aaeeff
Abhinandan Pakhare <abhinandanpakhare@gmail.com>
2026-07-13 21:01:25 +0530

    Update fin_agents.py

--- fin_agents.py
+++ fin_agents.py
@@ -1,15 +1,17 @@
 import streamlit as st
-from crewai import Agent
-from langchain_groq import ChatGroq
+from crewai import Agent, LLM
 from fin_tools import SearchTool
 import re
-
 
 # AGENTS
 class FinAgents():
 
     def llm(self):
-        llm = ChatGroq(model="mixtral-8x7b-32768", temperature=0, api_key=st.secrets['GROQ_API']) # model="llama3-70b-8192"
+        llm = LLM(
+            model="groq/llama-3.3-70b-versatile",
+            temperature=0,
+            api_key=st.secrets['GROQ_API']
+        )
         return llm
 
     # Financial expert
@@ -24,22 +26,22 @@ class FinAgents():
             max_iter=5,
             allow_delegation=False,
             tools=[SearchTool.fin_data_tool],
-            llm = self.llm(),
+            llm=self.llm(),
         )
-        
+
     # news expert
     def news_agent(self):
         return Agent(
             role="News Analyst",
             goal="Uncover news and market sentiments about the company {ticker}",
             backstory="""You work at an news office for asset management firm.
-          Your goal is to get and categorize news of the stock {ticker} and identify trends.""",
+            Your goal is to get and categorize news of the stock {ticker} and identify trends.""",
             verbose=True,
             memory=True,
             max_iter=5,
             allow_delegation=False,
             tools=[SearchTool.search_web_tool],
-            llm = self.llm(),
+            llm=self.llm(),
         )
 
     # reporter expert
@@ -48,32 +50,27 @@ class FinAgents():
             role="Financial Reporter",
             goal="Craft compelling and detailed content on financial report for the stock {ticker}",
             backstory="""You are a renowned Content Strategist for {ticker}, known for your insightful and engaging articles.
-          You transform complex concepts into compelling narratives.""",
+            You transform complex concepts into compelling narratives.""",
             verbose=True,
             memory=True,
             max_iter=5,
             allow_delegation=False,
-            llm = self.llm()
+            llm=self.llm()
         )
 
 
-
-
 class StreamToExpander:
-    # Print agent process to Streamlit app container 
-    # This portion of the code is adapted from @AbubakrChan; thank you!  
-
+    # Print agent process to Streamlit app container
+    # This portion of the code is adapted from @AbubakrChan; thank you!
     def __init__(self, expander):
         self.expander = expander
         self.buffer = []
-        self.colors = ['red', 'green', 'blue', 'orange']  # Define a list of colors
-        self.color_index = 0  # Initialize color index
+        self.colors = ['red', 'green', 'blue', 'orange']
+        self.color_index = 0
 
     def write(self, data):
-        # Filter out ANSI escape codes using a regular expression
         cleaned_data = re.sub(r'\x1B\[[0-9;]*[mK]', '', data)
 
-        # Check if the data contains 'task' information
         task_match_object = re.search(r'\"task\"\s*:\s*\"(.*?)\"', cleaned_data, re.IGNORECASE)
         task_match_input = re.search(r'task\s*:\s*([^\n]*)', cleaned_data, re.IGNORECASE)
         task_value = None
@@ -81,19 +78,13 @@ class StreamToExpander:
             task_value = task_match_object.group(1)
         elif task_match_input:
             task_value = task_match_input.group(1).strip()
-
         if task_value:
             st.toast(":robot_face: " + task_value)
 
-        # Check if the text contains the specified phrase and apply color
         if "Entering new CrewAgentExecutor chain" in cleaned_data:
-            # Apply different color and switch color index
-            self.color_index = (self.color_index + 1) % len(self.colors)  # Increment color index and wrap around if necessary
-
+            self.color_index = (self.color_index + 1) % len(self.colors)
             cleaned_data = cleaned_data.replace("Entering new CrewAgentExecutor chain", f":{self.colors[self.color_index]}[Entering new CrewAgentExecutor chain]")
-
         if "City Selection Expert" in cleaned_data:
-            # Apply different color 
             cleaned_data = cleaned_data.replace("City Selection Expert", f":{self.colors[self.color_index]}[City Selection Expert]")
         if "Local Expert at this city" in cleaned_data:
             cleaned_data = cleaned_data.replace("Local Expert at this city", f":{self.colors[self.color_index]}[Local Expert at this city]")
